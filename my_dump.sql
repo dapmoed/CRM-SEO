367f113293532f082c485daf54463e2cd8a3ee03
Konstantin Brikalov (aka Sloin) <dapmoed@gmail.com>
2013-07-11 12:30:18 +0700

    Test MySqlDump 2

--- my_dump.sql
+++ my_dump.sql
@@ -14,30 +14,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `test_text`
---
-
-DROP TABLE IF EXISTS `test_text`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `test_text` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `test_text`
---
-
-LOCK TABLES `test_text` WRITE;
-/*!40000 ALTER TABLE `test_text` DISABLE KEYS */;
-INSERT INTO `test_text` VALUES (1,'sfhsd'),(2,'2222'),(3,'dfgjfgj'),(4,'fgjdfgj'),(5,'dghdfh');
-/*!40000 ALTER TABLE `test_text` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -48,4 +24,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-07-11 12:28:20
+-- Dump completed on 2013-07-11 12:30:18
